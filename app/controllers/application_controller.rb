9285a28171b51f14175173f3b1cd788fcbc2c204
Ezra <njerukithinji872@gmail.com>
2023-05-09 15:52:32 +0300

    user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
 include ActionController::Cookies
 
 def authorize
+    puts "Authorization request failed"
     render json: {errors: "Please login first"}, status: :unauthorized unless session.include? :user_id
 end
 end
